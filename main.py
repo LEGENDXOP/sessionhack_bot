dcf9b13e43925b3358cb04d1afd1e93e66ccf0d2
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-10-23 11:47:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
+os.system("git clone https://github.com/Javes786/sessionhack_bot")
 os.system("cd sessionhack_bot && python bot.py")
